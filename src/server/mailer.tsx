14b522875794ab33bf20bd80d0e63ea0bbb21f84
Aarav Agarwal <aarav.knp.08@gmail.com>
2024-11-20 10:26:56 +0530

    Update mailer.tsx

--- src/server/mailer.tsx
+++ src/server/mailer.tsx
@@ -37,12 +37,17 @@ export const sendMail = async (email: string, emailType: string) => {
       html: `
         <h1>Email Verification</h1>
         <p>Please click the link below to verify your email address.</p>
-        <a href="${process.env.PUBLIC_URL}signup/verifyemail?token=${token}">Verify Email</a>
+        <a href="${process.env.PUBLIC_URL}/signup/verifyemail?token=${token}">Verify Email</a>
         `,
     };
     const mailResponse = await transporter.sendMail(mailOptions);
     return mailResponse;
-  } catch (error) {
-    console.error(error);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error("Failed to sign in user:", err.message);
+    } else {
+      console.error("Failed to sign in user: An unknown error occurred");
+    }
+    throw err;
   }
 };
